c5d5d1805928ab2d0ae11819bd7e78263f710c15
salbuque <salbuquerque@ucsb.edu>
2021-04-26 21:53:09 -0700

    completed Latin Hyper Cube Sensitivity analysis and second precipitation coefficient is a larger contributer to annual crop yield anomaly uncertainty.

--- almond_yield_model_SA.R
+++ almond_yield_model_SA.R
@@ -3,20 +3,21 @@
 #' This function computes Almond Yield from a dataframe of monthly climate data
 #’ Monthly precipitation and temperature will have an significant effect of yield
 #' @param climate_data_df A data frame of inputs including but not limited to the following columns: numeric day, numeric month, year, tmin_c, and precip 
-#' @param coeff_t First Coefficient of Average Monthly Temperature Value is -0.015
+#' @param coeff_t1 First Coefficient of Average Monthly Temperature Value is -0.015
 #' @param coeff_t2 Second Coefficient of Average Monthly Temperature Value squared is -0.0046
-#' @param coeff_p First Coefficient of Average Total Precipitation Value is -0.07
+#' @param coeff_p1 First Coefficient of Average Total Precipitation Value is -0.07
 #' @param coeff_p2 Second Coefficient of Average Total Precipitation Value squared is 0.0043
 #' @param constant 0.28
 #' @author Becca Reynolds, Madeline Oliver, Siya Qiu, and Simone Albuquerque
 #' @examples almond_yield_model(almond_yield_model(climate_data_df = climate_data))
+#' @references D.B. Lobell et al. Agricultural and Forest Meteorology 141 (2006) 208–218.
 #' @return Dataframe of summary statistics for sensitivity analysis and year and Almond Yield
-almond_yield_model = function(#add climate_data_df
+almond_yield_model_SA = function(#add climate_data_df
                               climate_data_df, 
                               #add default parameters based on equation given in Lobell
-                              coeff_t = -0.015, 
+                              coeff_t1 = -0.015, 
                               coeff_t2 = -0.0046, 
-                              coeff_p = -0.07,
+                              coeff_p1 = -0.07,
                               coeff_p2 = 0.0043,
                               constant = 0.28)
   {#function including data frame sub-setting, temperature, precipitation, and finally the equation
@@ -43,10 +44,9 @@ almond_yield_model = function(#add climate_data_df
   
    #add a column for each year's yield using the function from Lobell 
   yield_result_df <- full_join(mean_tmin_c_month2, sum_precip_month1) %>% 
-    mutate(almond_yield = (coeff_t*mean_tmin_c) + (coeff_t2*(mean_tmin_c^2)) + coeff_p*sum_precip + (coeff_p2*(sum_precip^2)) + constant) %>% 
-    mutate(mean = mean(almond_yield)) %>%
-    mutate(sd = sd(almond_yield)) %>% 
-    select(year, almond_yield, mean, sd) 
+    mutate(almond_yield = (coeff_t1*mean_tmin_c) + (coeff_t2*(mean_tmin_c^2)) + coeff_p1*sum_precip + (coeff_p2*(sum_precip^2)) + constant) %>% 
+    summarise(mean = mean(almond_yield), sd = sd(almond_yield)) %>% 
+    select(mean, sd) 
   return(yield_result_df)
   
 }
